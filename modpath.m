af41b527007041b47abf2b7d5ed48621a5b7510a
taylora <taylora@75a711ff-f5d0-4f92-a25f-37fcfe76b1f4>
2011-09-23 16:31:18 +0000

    Added modpath.m that adds TMT 1.10 to Matlab path (will have to be modified per-install)

---
+++ modpath.m
@@ -0,0 +1,17 @@
+% add toolbox to path
+addpath(genpath('~/packages/taylor_matlab_toolbox/taylor_matlab_toolbox_v1p10'));
+
+% % add local toolbox
+% addpath(sprintf('%s/toolbox',pwd));
+
+% % get path to parent dir
+% pwd_save=pwd;
+% cd ..
+% parent=pwd;
+% cd(pwd_save);
+
+% % add toolbox dirs that live underneath the parent dir
+% addpath(sprintf('%s/toolbox',parent));
+% addpath(sprintf('%s/toolbox/fourier_stuff',parent));
+% addpath(sprintf('%s/toolbox/hds',parent));
+% addpath(sprintf('%s/toolbox/measure_spont',parent));
